f0347c6a93d9b3d8b6bb58dfce8d1c8b55c112ad
Paolo Carta <ing.pcarta@gmail.com>
2020-04-08 13:33:33 +0200

    adding setup scripts

---
+++ install-cli.sh
@@ -0,0 +1,11 @@
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # Get the tar.xz
+    curl -LO https://github.com/tektoncd/cli/releases/download/v0.8.0/tkn_0.8.0_Linux_x86_64.tar.gz
+    # Extract tkn to your PATH (e.g. /usr/local/bin)
+    sudo tar xvzf tkn_0.8.0_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
+  
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew tap tektoncd/tools
+    brew install tektoncd/tools/tektoncd-cli        # Mac OSX
+fi
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/paolocarta/spring-rest-tekton-pipeline.git
-
 chmod 777 ./install.sh
 ./install.sh
+
+git clone https://github.com/paolocarta/spring-rest-tekton-pipeline.git
 
 oc project workshop
